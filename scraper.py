7f06ff49cd4055cbb37b63112ff2f320dbdc4bc3
JpUsefRye <youssefheshamhassan@gmail.com>
2018-03-15 08:34:36 +0200

    arranged in 'twitter()' function

--- scraper.py
+++ scraper.py
@@ -32,38 +32,48 @@ try:
 except Exception as e:
     banner()
 
-source = requests.get('https://twitter.com/'+user)
 
-soup = BeautifulSoup(source.text, 'lxml')
+if 'https://twitter.com/' not in user:
+    source = requests.get('https://twitter.com/'+user)
+else:
+    source = request.get(user)
 
-# Get Information
-title = soup.find('title').text
-try:
-    username = soup.find('a', class_='ProfileHeaderCard-nameLink').text
-except Exception as e:
-    bad_request()
-name_tag = soup.find('b', class_='u-linkComplex-target').text
-bio = soup.find('p', class_='ProfileHeaderCard-bio').text
-website = str(soup.find('span', class_='ProfileHeaderCard-urlText').text)
-website = website.replace(' ', '').replace('\n', '')
+def twitter(uname):
 
-# Get Image
-im_url = soup.find('img', class_='ProfileAvatar-image')['src']
-response = requests.get(im_url)
-img = Image.open(BytesIO(response.content))
+    soup = BeautifulSoup(source.text, 'lxml')
 
-# TODO: Show Image In Terminal Window
-# Resize Image
-#img = resize((28, 28), img)
+    # Get Information
+    title = soup.find('title').text
+    try:
 
-# Save Image
-img.save(f'{user}.png', 'png')
+        username = soup.find('a', class_='ProfileHeaderCard-nameLink').text
+    except Exception as e:
+        bad_request()
+    name_tag = soup.find('b', class_='u-linkComplex-target').text
+    bio = soup.find('p', class_='ProfileHeaderCard-bio').text
+    website = str(soup.find('span', class_='ProfileHeaderCard-urlText').text)
+    website = website.replace(' ', '').replace('\n', '') # i am lazy so...
 
-# Print To The Console
-print("\n-------------Console Output-------------")
-print(f"Title: {title}")
-print(f"Username: {username}")
-print(f"Name Tag: {name_tag}")
-print(f"\nBio: {bio}\n")
-print(f"Website: {website}")
-gui_show(img)
+    # Get Image
+    im_url = soup.find('img', class_='ProfileAvatar-image')['src']
+    response = requests.get(im_url)
+    img = Image.open(BytesIO(response.content))
+
+    # TODO: Show Image In Terminal Window
+    # Resize Image
+    #img = resize((28, 28), img)
+
+    # Save Image
+    img.save(f'{user}.png', 'png')
+
+    # Print To The Console
+    print("\n-------------Console Output-------------")
+    print(f"Title: {title}")
+    print(f"Username: {username}")
+    print(f"Name Tag: {name_tag}")
+    print(f"\nBio: {bio}\n")
+    print(f"Website: {website}")
+    gui_show(img)
+
+if __name__ == '__main__':
+    twitter(user)
